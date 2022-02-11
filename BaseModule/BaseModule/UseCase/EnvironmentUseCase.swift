605d34d5adafa1018fcefd095ce4f25f8c4725f4
Ernest Hong <hsh3592@gmail.com>
2022-02-11 17:53:13 +0900

    Group Channel - Logger

---
+++ BaseModule/BaseModule/UseCase/EnvironmentUseCase.swift
@@ -0,0 +1,20 @@
+//
+//  EnvironmentUseCase.swift
+//  BaseModule
+//
+//  Created by Ernest Hong on 2022/02/11.
+//
+
+import Foundation
+import SendBirdSDK
+
+public class EnvironmentUseCase {
+    
+    private static let applicationId: String = "A74A3E6C-ECE4-410C-AA5D-69D397B1EA73"
+    
+    public static func initializeSendbirdSDK() {
+        SBDMain.initWithApplicationId(applicationId)
+        SBDMain.setLogLevel([.error, .info])
+    }
+    
+}
