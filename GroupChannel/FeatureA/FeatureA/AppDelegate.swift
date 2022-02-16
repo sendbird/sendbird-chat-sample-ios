ffc05f637b97c21c086cdeed422f086af384ef6b
Ernest Hong <hsh3592@gmail.com>
2022-02-16 18:42:25 +0900

    Add FeatureA

---
+++ GroupChannel/FeatureA/FeatureA/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  BasicGruopChannel
+//
+//  Created by Ernest Hong on 2022/02/08.
+//
+
+import UIKit
+import SendBirdSDK
+import BaseModule
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        EnvironmentUseCase.initializeSendbirdSDK()
+        BaseAppearance.apply()
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
