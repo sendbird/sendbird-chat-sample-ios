bd7fe9797e1c7974fd01351c68307dadcba8d8b9
Ernest Hong <hsh3592@gmail.com>
2022-02-23 22:26:00 +0900

    Add IncomingImageCell

--- GroupChannel/BasicGroupChannel/BasicGroupChannel/Channel/Cell/GroupChannelIncomingImageCell.swift
+++ GroupChannel/BasicGroupChannel/BasicGroupChannel/Channel/Cell/GroupChannelIncomingImageCell.swift
@@ -11,32 +11,47 @@ import Kingfisher
 
 final class GroupChannelIncomingImageCell: UITableViewCell, GroupChannelFileCell {
     
-    @IBOutlet private weak var messageTextLabel: UILabel!
+    @IBOutlet private weak var profileImageView: UIImageView!
+    @IBOutlet private weak var nicknameTextLabel: UILabel!
     @IBOutlet private weak var messageImageView: UIImageView!
-    
-    private weak var message: SBDFileMessage?
+    @IBOutlet private weak var placeholderImageView: UIImageView!
     
     override func prepareForReuse() {
         super.prepareForReuse()
         
-        message = nil
-        messageTextLabel.text = nil
-        
+        nicknameTextLabel.text = nil
+        profileImageView.kf.cancelDownloadTask()
+        profileImageView.image = nil
         messageImageView.kf.cancelDownloadTask()
-        messageImageView.image = UIImage(named: "img_icon_general_file")
+        messageImageView.image = nil
     }
     
     func configure(with message: SBDFileMessage) {
-        self.message = message
-        
-        messageTextLabel.text = "(FILE) \(message.name)"
-        
-        if let thumbnailURLString = message.thumbnails?.first?.url,
-           let thumbnailURL = URL(string: thumbnailURLString) {
-            messageImageView.kf.setImage(with: thumbnailURL)
-        } else if message.type.hasPrefix("image"), let imageURL = URL(string: message.url) {
-            messageImageView.kf.setImage(with: imageURL)
+        if let sender = message.sender {
+            nicknameTextLabel.text = "\(sender.nickname ?? "(Unknown)")"
+            profileImageView.setProfileImageView(for: sender)
         }
+        
+        guard let imageURL = imageURL(for: message) else { return }
+        
+        messageImageView.kf.setImage(with: imageURL) { [weak self] result in
+            switch result {
+            case .success:
+                self?.placeholderImageView.isHidden = true
+            case .failure:
+                self?.placeholderImageView.isHidden = false
+            }
+        }
+    }
+    
+    private func imageURL(for message: SBDFileMessage) -> URL? {
+        if let thumbnailURLString = message.thumbnails?.first?.url {
+            return URL(string: thumbnailURLString)
+        } else if message.type.hasPrefix("image") {
+            return URL(string: message.url)
+        }
+        
+        return nil
     }
 
 }
