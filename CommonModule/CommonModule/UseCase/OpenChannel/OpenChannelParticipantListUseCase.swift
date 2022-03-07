cb8ebd7d60c65d1b27afb88dac9c525debf2f0a1
Ernest Hong <hsh3592@gmail.com>
2022-03-07 10:56:08 +0900

    [CORE-1266] Sample App - Retrieve a list of participants in open channel

---
+++ CommonModule/CommonModule/UseCase/OpenChannel/OpenChannelParticipantListUseCase.swift
@@ -0,0 +1,50 @@
+//
+//  OpenChannelParticipantListUseCase.swift
+//  CommonModule
+//
+//  Created by Ernest Hong on 2022/03/07.
+//
+
+import Foundation
+import SendBirdSDK
+
+public protocol OpenChannelParticipantListUseCaseDelegate: AnyObject {
+    func openChannelParticipantListUseCase(_ useCase: OpenChannelParticipantListUseCase, didReceiveError error: SBDError)
+    func openChannelParticipantListUseCase(_ useCase: OpenChannelParticipantListUseCase, didUpdateParticipants participants: [SBDUser])
+}
+
+open class OpenChannelParticipantListUseCase {
+    
+    public weak var delegate: OpenChannelParticipantListUseCaseDelegate?
+    
+    public private(set) var participants: [SBDUser] = []
+    
+    private let channel: SBDOpenChannel
+    
+    private lazy var participantListQuery = channel.createParticipantListQuery()
+    
+    public init(channel: SBDOpenChannel) {
+        self.channel = channel
+        
+        channel.createParticipantListQuery()
+    }
+    
+    open func loadNextPage() {
+        guard let participantListQuery = participantListQuery,
+              participantListQuery.isLoading() == false,
+              participantListQuery.hasNext else { return }
+        
+        participantListQuery.loadNextPage { [weak self] participants, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                self.delegate?.openChannelParticipantListUseCase(self, didReceiveError: error)
+                return
+            }
+            
+            self.participants.append(contentsOf: participants ?? [])
+            self.delegate?.openChannelParticipantListUseCase(self, didUpdateParticipants: self.participants)
+        }
+    }
+        
+}
