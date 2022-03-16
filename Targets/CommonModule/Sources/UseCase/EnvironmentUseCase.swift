9bda4d39985146f662960f39d5c70ecb4d336fdb
Ernest Hong <hsh3592@gmail.com>
2022-03-16 17:02:02 +0900

    Update SendbirdChat.initialize

--- Targets/CommonModule/Sources/UseCase/EnvironmentUseCase.swift
+++ Targets/CommonModule/Sources/UseCase/EnvironmentUseCase.swift
@@ -13,8 +13,13 @@ public class EnvironmentUseCase {
     private static let applicationId: String = "A74A3E6C-ECE4-410C-AA5D-69D397B1EA73"
     
     public static func initializeSendbirdSDK() {
-        SBDMain.initWithApplicationId(applicationId)
-        SBDMain.setLogLevel([.error, .info])
+        let initParams = InitParams(
+            applicationId: applicationId,
+            isLocalCachingEnabled: false,
+            logLevel: .error,
+            appVersion: "1.0.0"
+        )
+        SendbirdChat.initialize(params: initParams)
     }
     
 }
