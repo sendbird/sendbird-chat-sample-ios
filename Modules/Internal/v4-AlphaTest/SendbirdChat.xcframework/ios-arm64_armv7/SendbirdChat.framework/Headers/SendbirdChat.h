e863bb5f6f3cf5eb60dfbf5775ca13d2646f58ed
Ernest Hong <hsh3592@gmail.com>
2022-03-23 16:34:44 +0900

    Add xcframework file

---
+++ Modules/Internal/v4-AlphaTest/SendbirdChat.xcframework/ios-arm64_armv7/SendbirdChat.framework/Headers/SendbirdChat.h
@@ -0,0 +1,18 @@
+//
+//  SendbirdChat.h
+//  SendbirdChat
+//
+//  Created by Minhyuk Kim on 2021/07/08.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SendbirdChat.
+FOUNDATION_EXPORT double SendbirdChatVersionNumber;
+
+//! Project version string for SendbirdChat.
+FOUNDATION_EXPORT const unsigned char SendbirdChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SendbirdChat/PublicHeader.h>
+
+
