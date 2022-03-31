055cad69ec69e5f2c6695ad5fb04644ecdfe68d1
Ernest Hong <hsh3592@gmail.com>
2022-03-31 18:02:03 +0900

    Refactor `SettingViewController.xib` to code

--- Apps/BasicGroupChannel/Sources/Setting/SettingViewController.swift
+++ Apps/BasicGroupChannel/Sources/Setting/SettingViewController.swift
@@ -10,8 +10,30 @@ import CommonModule
 
 public final class SettingViewController: UIViewController {
     
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    private lazy var editUserProfileButton: UIButton = {
+        let editUserProfileButton = UIButton(frame: .zero)
+        editUserProfileButton.addTarget(self, action: #selector(didTouchEditUserProfileButton(_:)), for: .touchUpInside)
+        editUserProfileButton.setTitle("Edit User Profile", for: .normal)
+        editUserProfileButton.setTitleColor(.systemBlue, for: .normal)
+        return editUserProfileButton
+    }()
+    
+    private lazy var logoutButton: UIButton = {
+        let logoutButton = UIButton(frame: .zero)
+        logoutButton.addTarget(self, action: #selector(didTouchLogoutButton(_:)), for: .touchUpInside)
+        logoutButton.setTitle("Logout", for: .normal)
+        logoutButton.setTitleColor(.systemRed, for: .normal)
+        return logoutButton
+    }()
+    
     public init() {
-        super.init(nibName: "SettingViewController", bundle: Bundle(for: Self.self))
+        super.init(nibName: nil, bundle: nil)
         title = "Setting"
     }
     
@@ -21,17 +43,27 @@ public final class SettingViewController: UIViewController {
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+        ])
+        
+        stackView.addArrangedSubview(editUserProfileButton)
+        stackView.addArrangedSubview(logoutButton)
     }
     
-    @IBAction private func didTouchEditUserProfileButton(_ sender: Any) {
+    @objc private func didTouchEditUserProfileButton(_ sender: Any) {
         let profileEditViewController = ProfileEditViewController()
         let navigation = UINavigationController(rootViewController: profileEditViewController)
         
         present(navigation, animated: true)
     }
-        
     
-    @IBAction private func didTouchLogoutButton(_ sender: UIButton) {
+    @objc private func didTouchLogoutButton(_ sender: UIButton) {
         UserConnectionUseCase.shared.logout { [weak self] in
             self?.dismiss(animated: true)
         }
