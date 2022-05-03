91da3ac6414f54df5cd8ef3e61f55b29024d2b3b
Yogesh <veeraraj.yogesh@gmail.com>
2022-05-03 15:47:02 +0200

    Added sample and use case

---
+++ Apps/CategorizeWithCustomTypeOpenChannel/Sources/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BasicOpenChannel
+//
+//  Created by Ernest Hong on 2022/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
