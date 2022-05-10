5d97d4aac4082d4d855d2daa9a139a5d3cc8d709
Yogesh <veeraraj.yogesh@gmail.com>
2022-05-10 14:41:56 +0200

    Added option to upload file

--- Apps/SendVariousTypesOfFileOpenChannel/Sources/Channel/OpenChannelViewController+FileMessage.swift
+++ Apps/SendVariousTypesOfFileOpenChannel/Sources/Channel/OpenChannelViewController+FileMessage.swift
@@ -12,7 +12,24 @@ import CommonModule
 extension OpenChannelViewController: ImagePickerRouterDelegate {
 
     func presentAttachFileAlert() {
-        imagePickerRouter.presentAlert()
+        let actionSheet = UIAlertController(title: "Choose", message: nil, preferredStyle: .actionSheet)
+        actionSheet.addAction(
+            UIAlertAction(title: "Choose Album", style: .default) { [weak self] _ in
+                self?.imagePickerRouter.presentAlert()
+            }
+        )
+
+        actionSheet.addAction(
+            UIAlertAction(title: "Upload PDF", style: .destructive) { [weak self] _ in
+                self?.sendPDFFile()
+            }
+        )
+        
+        actionSheet.addAction(
+            UIAlertAction(title: "Cancel", style: .cancel)
+        )
+
+        present(actionSheet, animated: true, completion: nil)
     }
     
     func imagePickerRouter(_ imagePickerRouter: ImagePickerRouter, didFinishPickingMediaFile mediaFile: ImagePickerMediaFile) {
@@ -32,6 +49,35 @@ extension OpenChannelViewController: ImagePickerRouterDelegate {
 
         targetMessageForScrolling = sendingMessage
         messageListUseCase.didStartSendMessage(sendingMessage)
+    }
+    
+    func sendPDFFile() {
+        var sendingMessage: BaseMessage?
+        guard let url = Bundle.main.url(
+            forResource: "Sample_Sendbird",
+            withExtension: "pdf"
+        ) else {
+            return
+        }
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        
+        sendingMessage = fileMessageUseCase.sendFile(.init(data: data, name: "Sample_Sendbird.pdf", mimeType: "application/pdf")) { [weak self] result in
+            switch result {
+            case .success(let message):
+                self?.targetMessageForScrolling = message
+                self?.messageListUseCase.didSuccessSendMessage(message)
+            case .failure(let error):
+                self?.targetMessageForScrolling = nil
+                self?.messageListUseCase.didFailSendMessage(sendingMessage)
+                self?.presentAlert(error: error)
+            }
+        }
+
+        targetMessageForScrolling = sendingMessage
+        messageListUseCase.didStartSendMessage(sendingMessage)
+
     }
     
 }
