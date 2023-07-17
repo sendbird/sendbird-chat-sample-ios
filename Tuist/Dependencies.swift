f1a668b1dafb02a2e580ef490d7a74c389af0f5f
Jed Gyeong <jed@sendbird.com>
2023-07-17 10:53:07 +0900

    Updated Kingfisher for the latest Xcode. Updated the latest version of SendbirdChatSDK.

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -9,8 +9,8 @@ import ProjectDescription
 
 let dependencies = Dependencies(
     swiftPackageManager: [
-        .remote(url: "https://github.com/sendbird/sendbird-chat-sdk-ios", requirement: .upToNextMinor(from: "4.6.3")),
-        .remote(url: "https://github.com/onevcat/Kingfisher", requirement: .upToNextMinor(from: "7.2.0")),
+        .remote(url: "https://github.com/sendbird/sendbird-chat-sdk-ios", requirement: .upToNextMinor(from: "4.9.5")),
+        .remote(url: "https://github.com/onevcat/Kingfisher", requirement: .upToNextMinor(from: "7.8.1")),
     ],
     platforms: [.iOS]
 )
